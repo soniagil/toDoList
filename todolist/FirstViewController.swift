d185cbb0cb6c9846e57be72dc5abbe2eee8f8bbc
Sonia Gil <sgilbenitez@gmail.com>
2016-05-05 12:29:24 +0200

    base project

---
+++ todolist/FirstViewController.swift
@@ -0,0 +1,49 @@
+//
+//  FirstViewController.swift
+//  todolist
+//
+//  Created by Sonia Gil Benítez on 5/5/16.
+//  Copyright © 2016 Sonia Gil Benítez. All rights reserved.
+//
+
+import UIKit
+
+// se añade fuera de la clase para poder acceder a ella desde el segundo view controller
+var toDoList = [String]()
+
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var toDoLisTable: UITableView!
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        toDoLisTable.reloadData()
+    }    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoList.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = toDoList[indexPath.row]
+        
+        return cell
+    }
+
+
+}
+
---
+++ todolist/SecondViewController.swift
@@ -0,0 +1,46 @@
+//
+//  SecondViewController.swift
+//  todolist
+//
+//  Created by Sonia Gil Benítez on 5/5/16.
+//  Copyright © 2016 Sonia Gil Benítez. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var item: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        item.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        super.touchesBegan(touches, withEvent: event)
+        
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+
+    @IBAction func addItem(sender: AnyObject) {
+        toDoList.append(item.text!)
+        
+        item.text = ""
+    }
+
+}
+
